1f5541a488481adf0090a1b4bec95c52b1cfc224
Claudia Luque Fernández <claudia.luque.fernandez@gmail.com>
2019-03-17 01:27:21 +0000

    Complete retrofit config and datasource implmentation

--- search/src/main/java/dev/claucookielabs/search/data/datasource/TracksDatasource.kt
+++ search/src/main/java/dev/claucookielabs/search/data/datasource/TracksDatasource.kt
@@ -1,14 +1,24 @@
 package dev.claucookielabs.search.data.datasource
 
+import dev.claucookielabs.search.data.datasource.remote.SearchApi
 import dev.claucookielabs.search.data.model.api.ApiTrackInfo
+import io.reactivex.Single
+import javax.inject.Inject
 
 interface TracksDatasource {
-    fun getTracksByName(trackName: String): List<ApiTrackInfo>
+    fun getTracksByName(trackName: String): Single<List<ApiTrackInfo>>
 }
 
 class TracksDatasourceImpl : TracksDatasource {
-    override fun getTracksByName(trackName: String): List<ApiTrackInfo> {
-        return listOf()
+
+    @Inject
+    lateinit var searchApi: SearchApi
+
+    override fun getTracksByName(trackName: String): Single<List<ApiTrackInfo>> {
+        return searchApi.listTracksByName(trackName, "9556cfb87fe52e0f72bdaa3163dda5cf")
+            .map {
+                it.results.trackMatches.tracks
+            }
     }
 
 }
--- search/src/main/java/dev/claucookielabs/search/di/SearchApiModule.kt
+++ search/src/main/java/dev/claucookielabs/search/di/SearchApiModule.kt
@@ -1,2 +1,45 @@
 package dev.claucookielabs.search.di
 
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dev.claucookielabs.search.data.datasource.remote.SearchApi
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+
+@Module
+class SearchApiModule {
+
+    @Singleton
+    @Provides
+    fun provideRestApi(okHttpClient: OkHttpClient): SearchApi {
+
+        val gson = GsonBuilder()
+            .setLenient()
+            .create()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://ws.audioscrobbler.com/2.0/")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .client(okHttpClient)
+            .build()
+
+        return retrofit.create(SearchApi::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttp(): OkHttpClient {
+        return OkHttpClient
+            .Builder()
+            .callTimeout(10, TimeUnit.SECONDS)
+            .build()
+    }
+
+}
