2695584bea24b9991531119039cc0a363b410f65
Claudia Luque Fernández <claudia.luque.fernandez@gmail.com>
2019-03-19 23:21:34 +0000

    Customize cache and add transitions to image loader

--- core/src/main/java/dev/claucookielabs/core/utils/ImageLoader.kt
+++ core/src/main/java/dev/claucookielabs/core/utils/ImageLoader.kt
@@ -4,10 +4,15 @@ import android.view.View
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import dev.claucookielabs.core.R
 
 fun View.loadImage(url: String, imageView: ImageView) {
     Glide.with(context)
         .load(url)
-        .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+        .placeholder(R.drawable.ic_launcher_background)
+        .error(R.drawable.ic_launcher_background)
+        .transition(DrawableTransitionOptions.withCrossFade())
+        .diskCacheStrategy(DiskCacheStrategy.ALL)
         .into(imageView)
 }
