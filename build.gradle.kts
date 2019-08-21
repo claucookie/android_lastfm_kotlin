9b6784516fc733954b81facc5b51e0502d38945f
Claudia Luque Fernandez <claudia.luque.fernandez@gmail.com>
2019-08-21 21:07:56 +0100

    Migrate main build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,18 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext.kotlin_version = '1.3.10'
-    ext.dagger_version = '2.21'
     repositories {
         google()
         jcenter()
         mavenCentral()
-        maven { url 'https://maven.google.com' }
     }
+
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.5.0-rc03'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:3.5.0-rc03")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.10")
         // There is an issue with androidx and dagger 2.20. Beta version solves it
         // https://github.com/google/dagger/issues/1371#issuecomment-449748217
-        classpath 'com.android.tools.build.jetifier:jetifier-processor:1.0.0-beta02'
+        classpath("com.android.tools.build.jetifier:jetifier-processor:1.0.0-beta02")
     }
 }
 
@@ -26,6 +24,6 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
